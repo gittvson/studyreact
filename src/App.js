e65e1a80d923002b1a97e29e97d9057e6db37531
tranvanson <son.tv1509@gmail.com>
2020-12-28 21:04:30 +0700

    first change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useState } from 'react'
+import React from 'react'
+const App=(props)=>{
+  const [num,setNum]=useState(1);
+  const handleClick=()=>{
+    setNum(num+1);
+  }
+  var arr=[56,1,3,5,1,86,12,86];
+  arr.sort((a,b)=>{
+    return a-b;
+  })
+  return(
+    <div>
+    <button onClick={()=>handleClick()}>Click here</button>
+    <h1>{num}</h1>
+  <h1>{arr.map((num)=>{return num+" ";})}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
